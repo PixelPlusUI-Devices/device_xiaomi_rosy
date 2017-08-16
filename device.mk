417ea5f2dfcdc1ab651881d453049ee8f4e8728f
Abodunrinwa Toki <toki@google.com>
2017-08-16 21:32:06 +0100

    rosy: Remove textclassifier models
    This reverts commit 5d371ef52ac63423048ea906c4276893d4b04424, e55acd92ddf94286e559bf6bea71e7a7cc89e587.
    
    ConfigUpdateInstallReceiver will be used to push an updatable
    model to the devices.
    
    Test: Checked that a new build does not bundle any of the bundle1
    models (i.e. en, es, de, fr) by default.
    
    Bug: 64746048
    Change-Id: I507aa8d5911da8ae27f951fe566bb86c1a10238b

--- device.mk
+++ device.mk
@@ -382,10 +382,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
 
-# TextClassifier smart selection model files
-PRODUCT_PACKAGES += \
-    textclassifier.bundle1
-
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
