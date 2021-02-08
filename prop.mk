de6679aa119b214d0a324feae73bf04d36ee180a
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-02-08 15:58:04 +0000

    rosy: props: enable blur

--- prop.mk
+++ prop.mk
@@ -45,6 +45,10 @@ vendor.qcom.bluetooth.soc=smd \
 ro.bluetooth.hfp.ver=1.7 \
 ro.qualcomm.bt.hci_transport=smd
 
+# Blur
+ro.sf.blurs_are_expensive=1
+ro.surface_flinger.supports_background_blur=1
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 camera.display.lmax=1280x720 \
